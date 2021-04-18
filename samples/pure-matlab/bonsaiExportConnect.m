d1386aef5a39ea50cf370614c3ad16a3fc8a7f95
Forrest Trepte <forrest@microsoft.com>
2021-04-18 16:17:06 -0700

    tidy up remnants of cartpole sample

--- samples/pure-matlab/bonsaiExportConnect.m
+++ samples/pure-matlab/bonsaiExportConnect.m
@@ -1,18 +1,5 @@
-% Script to configure exported Bonsai brain. Exported brains can be
-% run in a few steps:
-%   1. export your brain
-%   2. start exported brain
-%   3. configure the endpoint (defaults to localhost:5000)
-%   4. run this script to configure an export session and set required variables
-%   5. open the model and click "Run"
+% Copyright (c) Microsoft Corporation.
+% Licensed under the MIT License.
 
-mdl = 'cartpole_discrete';
-load_system(mdl);
-set_param(mdl, 'FastRestart', 'off');
-
-% configure exported brain
-config = bonsaiConfig;
-BonsaiConfigureExportConnect(config, 'NoSimulink);
-
-% any initial data required for compilation should go here
-initialPos = 0;
+% The export connect mode (viewing an exported brain running in Simulink)
+% is not supported in this pure MATLAB sample.
--- samples/pure-matlab/foo.m
+++
@@ -1,7 +0,0 @@
-function [reward, observation1, observation2] = foo(action_array1, action_array2,config_array3)
-% array 1 is an action of length 10
-% array 2 is an action of length 20 
-observation1 = action_array1*2;
-observation2 = action_array2*3;
-reward = sum(observation1) + sum(observation2) + sum(config_array3)
-
